8517371e8bb0a6e624715dc94c44e7b41a1c9c67
IvanFan <3323730286@qq.com>
2024-04-28 22:25:36 +0800

    refactor: keyboard.cpp

--- src/battleship/keyboard.cpp
+++ src/battleship/keyboard.cpp
@@ -1,88 +1,79 @@
+#include "battleship/keyboard.h"
 #include <termios.h>
 #include <unistd.h>
-#include "battleship/keyboard.h"
-using namespace std;
-char keyboard(){
-	char key;
-	struct termios new_settings;
-	struct termios stored_settings;
-	tcgetattr(0, &stored_settings);
-	new_settings = stored_settings;
-	new_settings.c_lflag &= ~(ICANON | ECHO); //disable line buffering and echoing of characters
-	new_settings.c_cc[VTIME] = 0;
-	new_settings.c_cc[VMIN] = 0;
-	tcsetattr(0, TCSANOW, &new_settings);
 
-	bool exit = false;
-	while (!exit){ //prompt for input unless 
-		if (read(0, &key, 1) > 0) {
-			switch (key){
-				case ' '://Press the spacebar, and the orientation of the ship will be changed
-					key = 'C';
-					exit = true;
-					break;
-				case '\n'://Press the enter key, and the function will return character 'Y'
-					key = 'Y';
-					exit = true;
-					break;
-				case 'w': //moving upwards, denoted by 'w'
-				case 'W':
-					key = 'w';
-					exit = true;
-					break;
-				case 'A'://moving to the left, denoted by 'a'
-				case 'a':
-					key = 'a';
-					exit = true;
-					break;
-				case 's'://moving downwards, denoted by 's'
-				case 'S':
-					key = 's';
-					exit = true;
-					break;
-				case 'd'://moving to the right, denoted by 'd'
-				case 'D':
-					key = 'd';
-					exit = true;
-					break;
-				case 'q'://moving to the right, denoted by 'd'
-				case 'Q':
-					key = 'N';
-					exit = true;
-					break;
-				case '\033':  // process the arrow keys
-                    if (read(STDIN_FILENO, &key, 1) > 0 && key == '[') {
-                        if (read(STDIN_FILENO, &key, 1) > 0) {
-                            switch (key) {
-                                case 'A':
-                                    key = 'w';
-									exit = true;
-                                    break;
-                                case 'B':
-                                    key = 's';
-                                    exit = true;
-                                    break;
-                                case 'C':
-                                    key = 'd';
-                                    exit = true;
-                                    break;
-                                case 'D':
-                                    key = 'a';
-                                    exit = true;
-                                    break;
-                            }
-                        }
-                    }
-					else{	//ESC key pressed
-						key = 'N';
-						exit = true;
-						break;
-					}
-                    break;
-			}
-		}
-		
-	}
-	tcsetattr(0,TCSANOW, &stored_settings);//recover the original setting for input and display mode
-	return key;//return the character key, which can only be among {Y,N,w,a,s,d}
+char getKey();
+void disableBuffering(termios &oldSettings);
+void restoreSettings(const termios &oldSettings);
+
+using namespace std;
+
+char keyboard() {
+  termios oldSettings;
+  disableBuffering(oldSettings);
+
+  char key = getKey();
+
+  restoreSettings(oldSettings);
+  return key;
+}
+
+void disableBuffering(termios &oldSettings) {
+  termios newSettings;
+  tcgetattr(STDIN_FILENO, &oldSettings);
+  newSettings = oldSettings;
+  newSettings.c_lflag &= ~(ICANON | ECHO); // Disable line buffering and echoing
+  newSettings.c_cc[VTIME] = 0;
+  newSettings.c_cc[VMIN] = 1; // Wait for at least one character
+  tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);
+}
+
+void restoreSettings(const termios &oldSettings) {
+  tcsetattr(STDIN_FILENO, TCSANOW, &oldSettings);
+}
+
+char getKey() {
+  char key;
+  while (true) {
+    read(STDIN_FILENO, &key, 1);
+    switch (key) {
+    case ' ':
+      return 'C';
+    case '\n':
+      return 'Y';
+    case 'w':
+    case 'W':
+      return 'w';
+    case 'a':
+    case 'A':
+      return 'a';
+    case 's':
+    case 'S':
+      return 's';
+    case 'd':
+    case 'D':
+      return 'd';
+    case 'q':
+    case 'Q':
+      return 'N';
+    case '\033':
+      if (read(STDIN_FILENO, &key, 1) == -1)
+        return 'N'; // ESC key pressed
+      if (key == '[') {
+        if (read(STDIN_FILENO, &key, 1) == -1)
+          return 'N';
+        switch (key) {
+        case 'A':
+          return 'w';
+        case 'B':
+          return 's';
+        case 'C':
+          return 'd';
+        case 'D':
+          return 'a';
+        }
+      }
+      break;
+    }
+  }
 }
